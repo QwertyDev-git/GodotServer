696ba79ab431e6f2343936976c8d01b3d58fe296
QwertyDev-git <qwerty.acc.183@gmail.com>
2025-08-13 21:00:39 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const WebSocket = require("ws");
+
+const PORT = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: PORT });
+
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+
+    ws.on("message", (message) => {
+        console.log("Received:", message.toString());
+
+        // Kirim balik ke client
+        ws.send("Echo: " + message);
+    });
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    });
+});
+
+console.log(`WebSocket server running on port ${PORT}`);
